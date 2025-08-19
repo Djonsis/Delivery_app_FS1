760c9826c34c8a9aa80d0d92987ca295f77a0b76
Евгений Кретинин (Djonsis) <liquidDjo@gmail.com>
2025-08-19 15:05:41 +0000

    [2025-08-19T15:03:35.352Z] [INFO] [LOG_FILE_ACTION] Log file has been cl

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,17 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY
+  ? JSON.parse(`"${process.env.FIREBASE_PRIVATE_KEY}"`)
+  : undefined;
+
+
 // Инициализация Firebase Admin SDK для серверных операций
 if (!getApps().length) {
   initializeApp({
     credential: cert({
       projectId: "fastbasket",
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: privateKey,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
     projectId: "fastbasket",
